b72deccdfea2f0400c7de225b5ad505e323aa325
jaderhenryk <jaderhenryk@gmail.com>
2024-02-06 20:22:41 -0300

    Commit gitignore

--- src/app.tsx
+++ src/app.tsx
@@ -1,28 +1,26 @@
+import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
-import logo from './assets/logo-nlw-expert.svg'
 
 export function App() {
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
-
+    <div className="mx-auto my-12 max-w-6xl space-y-6">
       <img src={logo} alt="nlw expert" />
       <form className="w-full">
-        <input 
+        <input
           type="text"
           placeholder="Busque suas notas..."
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
         />
       </form>
-      <div className="h-px bg-slate-700"/>
+      <div className="h-px bg-slate-700" />
 
-      <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
-        <NewNoteCard/>
-        
-        <NoteCard/>
-        <NoteCard/>
+      <div className="grid auto-rows-[250px] grid-cols-3 gap-6">
+        <NewNoteCard />
+
+        <NoteCard />
+        <NoteCard />
       </div>
     </div>
-
   )
 }
