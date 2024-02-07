b0180dfd75594077dadf8f0c1eaf4d7f16b6600f
jaderhenryk <jaderhenryk@gmail.com>
2024-02-07 19:37:20 -0300

    Adding form to save a new note or visualize an existent

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,10 @@ import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
 export function App() {
+  const note = {
+    date: new Date(),
+    content: 'Hello World',
+  }
   return (
     <div className="mx-auto my-12 max-w-6xl space-y-6">
       <img src={logo} alt="nlw expert" />
@@ -18,8 +22,7 @@ export function App() {
       <div className="grid auto-rows-[250px] grid-cols-3 gap-6">
         <NewNoteCard />
 
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={note} />
       </div>
     </div>
   )
